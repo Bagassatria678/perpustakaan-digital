41aaf63365e8a14a2946367edb4a5949a3596c2d
Bagassatria678 <bagassatriarr@gmail.com>
2024-11-12 14:52:43 +0700

    first commit

--- src/views/BookForm.js
+++ src/views/BookForm.js
@@ -5,27 +5,27 @@ export class BookForm {
 
   render(onSubmit) {
     this.formContainer.innerHTML = `
-          <div class="card form-card glassy">
-              <div class="card-body">
-                  <h2 class="h4 mb-3">Tambah/Edit Buku</h2>
-                  <form id="book-form">
-                      <div class="mb-3">
-                          <label for="title" class="form-label">Judul Buku</label>
-                          <input type="text" class="form-control rounded-pill" id="title" placeholder="Judul Buku" required>
-                      </div>
-                      <div class="mb-3">
-                          <label for="author" class="form-label">Penulis</label>
-                          <input type="text" class="form-control rounded-pill" id="author" placeholder="Nama Penulis" required>
-                      </div>
-                      <div class="mb-3">
-                          <label for="year" class="form-label">Tahun Terbit</label>
-                          <input type="number" class="form-control rounded-pill" id="year" placeholder="Tahun" required>
-                      </div>
-                      <button type="submit" class="btn btn-primary w-100 rounded-pill shadow-neumorphic">Simpan Buku</button>
-                  </form>
-              </div>
-          </div>
-      `;
+            <div class="card form-card glassy">
+                <div class="card-body">
+                    <h2 class="h4 mb-3">Tambah/Edit Buku</h2>
+                    <form id="book-form">
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Judul Buku</label>
+                            <input type="text" class="form-control rounded-pill" id="title" placeholder="Judul Buku" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="author" class="form-label">Penulis</label>
+                            <input type="text" class="form-control rounded-pill" id="author" placeholder="Nama Penulis" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="year" class="form-label">Tahun Terbit</label>
+                            <input type="number" class="form-control rounded-pill" id="year" placeholder="Tahun" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary w-100 rounded-pill shadow-neumorphic">Simpan Buku</button>
+                    </form>
+                </div>
+            </div>
+        `;
 
     this.formContainer.querySelector("form").addEventListener("submit", (e) => {
       e.preventDefault();
